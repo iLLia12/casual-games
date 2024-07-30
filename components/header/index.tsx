2647257b1ea72c15da9bedd10db35f66a48e10fa
illia goncharov <goncharovphp@gmail.com>
2024-07-30 13:08:16 -0700

    header styles fix

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,16 +1,24 @@
 "use client";
 import { useEffect } from "react";
+import Image from "next/image";
 
 function handleScroll() {
   const header = document.getElementById("header");
+  const headerLogo = document.getElementById("header-logo");
   if (window.scrollY > 50) {
     header?.classList.add("h-12");
     header?.classList.remove("h-20");
     header?.classList.add("text-xs");
+    header?.classList.remove("bg-black/65");
+    header?.classList.add("bg-black/25");
+    headerLogo?.classList.add("w-20");
   } else {
     header?.classList.remove("h-12");
     header?.classList.add("h-20");
     header?.classList.remove("text-xs");
+    header?.classList.add("bg-black/65");
+    header?.classList.remove("bg-black/25");
+    headerLogo?.classList.remove("w-20");
   }
 }
 
@@ -24,13 +32,22 @@ export default function HeaderComponent() {
   return (
     <header
       id="header"
-      className="transition-all main-header w-full md:flex h-20 justify-between items-center bg-black/25 z-50 fixed scrolling px-6"
+      className="transition-all main-header w-full md:flex h-20 justify-between items-center bg-black/65 z-50 fixed scrolling px-6"
     >
       <div className="flex justify-between w-full">
         <div>
-          <div>LOGO</div>
+          <div>
+            <Image
+              id="header-logo"
+              src="/img/logo.png"
+              width={150}
+              height={50}
+              alt="Picture of the author"
+              className="transition-all"
+            />
+          </div>
         </div>
-        <div className="flex">
+        <div className="flex items-center">
           <div className="pr-2">ABOT</div>
           <div className="pr-2">GAMES</div>
           <div>MENU</div>
