49610794ddac3c4345394a807c7a9914d54e4776
illia goncharov <goncharovphp@gmail.com>
2024-07-30 11:30:59 -0700

    footer color

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,3 +1,3 @@
 export default function FooterComponent() {
-  return <footer className=" bg-[#fdc991] h-36 footer">This is footer</footer>;
+  return <footer className="bg-amber-700 h-36 footer">This is footer</footer>;
 }
